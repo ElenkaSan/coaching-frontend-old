25c9823fc01595ae996eb7744b1276ca329b5543
Elenka^_^San <75818489+ElenkaSan@users.noreply.github.com>
2023-01-05 16:26:47 -0500

    adding logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+// import React, { useContext } from "react";
 // import { useNavigate } from "react-router-dom";
 
 import AppBar from "@mui/material/AppBar";
@@ -9,14 +10,17 @@ import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Link from "@mui/material/Link";
 // import { Link } from 'react-router-dom';
+import logo from '../img/logo.png';
 
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import { Button, useTheme } from "@mui/material";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import "./component.css";
+// import UserContext from "../auth/UserContext";
 
 export default function Header({ logout }) {
+  // const { isLoggedIn } = useContext(UserContext);
   const [anchorEl, setAnchorEl] = React.useState(null);
   const theme = useTheme();
 
@@ -43,10 +47,140 @@ export default function Header({ logout }) {
       <AppBar className="secondBox">
         <Toolbar sx={{ justifyContent: "space-around" }}>
           <Typography variant="h6" component="div" style={{ color: "skyblue" }}>
-            LOGO
+            <h3> <img src={logo} alt="logo" style={{ height:'30px', width:'30px'}} /> </h3>
           </Typography>
-
+          {/* {isLoggedIn
+                ? (<> */}
           <div>
+            {mobile ? (
+              <>
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  sx={{ mr: 2 }}
+                  onClick={handleMenu}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <Link href="/" type="HomePage">
+                    <MenuItem onClick={handleClose}>Home</MenuItem>
+                  </Link>
+                  <Link href="/aboutus" type="AboutUs">
+                    <MenuItem onClick={handleClose}>About Us</MenuItem>
+                  </Link>
+                  <Link href="/programs" type="Programs">
+                    <MenuItem onClick={handleClose}>
+                      Programs for Kids & Adults
+                    </MenuItem>
+                  </Link>
+                  <Link href="/business" type="Business">
+                    <MenuItem onClick={handleClose}>Business</MenuItem>
+                  </Link>
+                  <Link href="/publications" type="Publications">
+                    <MenuItem onClick={handleClose}>Publications</MenuItem>
+                  </Link>
+                  <Link href="/contact" type="Contact">
+                    <MenuItem onClick={handleClose}>Contact</MenuItem>
+                  </Link>
+                  <Link href="/profile" type="login">
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                  </Link>
+                  <Link href="/" type="logout">
+                    <MenuItem onClick={handleClick}>Logout</MenuItem>
+                  </Link>
+                </Menu>
+              </>
+            ) : (
+              <div className="navBar">
+                <Button
+                  component={Link}
+                  href="/"
+                  type="HomePage"
+                  variant="contained"
+                >
+                  Home
+                </Button>
+                <Button
+                  component={Link}
+                  href="/aboutus"
+                  type="AboutUs"
+                  variant="contained"
+                >
+                  About Us
+                </Button>
+                <Button
+                  component={Link}
+                  href="/programs"
+                  type="Programs"
+                  variant="contained"
+                >
+                  Programs for Kids & Adults
+                </Button>
+                <Button
+                  component={Link}
+                  href="/business"
+                  type="Business"
+                  variant="contained"
+                >
+                  Business
+                </Button>
+                <Button
+                  component={Link}
+                  href="/publications"
+                  type="Publications"
+                  variant="contained"
+                >
+                  Publications
+                </Button>
+                <Button
+                  component={Link}
+                  href="/profile"
+                  type="profile"
+                  Click={handleClose}
+                >
+                  Profile
+                </Button>
+                <Button
+                  component={Link}
+                  href="/"
+                  type="logout"
+                  Click={handleClose}
+                >
+                  Logout
+                </Button>
+                <Button
+                  component={Link}
+                  href="/contact"
+                  type="Contact"
+                  variant="contained"
+                >
+                  Contact
+                </Button>
+                <Button component={Link} variant="contained">
+                  Eng/Rus
+                </Button>
+              </div>
+            )}
+          </div>
+          {/* </>) : (<>
+            <div>
             {mobile ? (
               <>
                 <IconButton
@@ -100,12 +234,6 @@ export default function Header({ logout }) {
                   <Link href="/signup" type="signup">
                     <MenuItem onClick={handleClick}>SignUp</MenuItem>
                   </Link>
-                  <Link href="/profile" type="login">
-                    <MenuItem onClick={handleClose}>Profile</MenuItem>
-                  </Link>
-                  {/* <Link href="/" type="logout">
-                    <MenuItem onClick={handleClick}>Logout</MenuItem>
-                  </Link> */}
                 </Menu>
               </>
             ) : (
@@ -166,16 +294,6 @@ export default function Header({ logout }) {
                 >
                   Signup
                 </Button>
-
-                <Button
-                  component={Link}
-                  href="/profile"
-                  type="profile"
-                  Click={handleClose}
-                >
-                  Profile
-                </Button>
-
                 <Button
                   component={Link}
                   href="/contact"
@@ -190,6 +308,8 @@ export default function Header({ logout }) {
               </div>
             )}
           </div>
+            </>) */}
+                {/* } */}
         </Toolbar>
       </AppBar>
     </Box>
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,47 +1,28 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router";
-// import {Routes, Route, Navigate } from "react-router-dom";
-// import { BrowserRouter } from "react-router-dom";
-// import useLocalStorage from "./hooks/useLocalStorage";
-
-// import LoadingSpinner from "./common/LoadingSpinner";
-// import Api from "./api";
-// import UserContext from "./auth/UserContext";
-
-import Header from "./Header";
+import React, { useContext } from "react"
+import { Switch, Route, Redirect } from "react-router-dom"
 import Homepage from "../homepage/Homepage";
-import AboutUs from "./AboutUs";
-import Programs from "./Programs";
-import Business from "./Business";
-import Publications from "./Publications";
-import Contact from "./Contact";
+import AboutUs from "../components/AboutUs";
+import Programs from "../components/Programs";
+import Business from "../components/Business";
+import Publications from "../components/Publications";
+import Contact from "../components/Contact";
 
 import SignupForm from "../auth/SignupForm"
 import LoginForm from "../auth/LoginForm"
 import ProfileForm from "../auth/ProfileForm"
 import UserPage from "../auth/UserPage"
-// import UserContext from "../auth/UserContext"
+import UserContext from "../auth/UserContext"
+import WeatherPage from "../WeatherPage/WeatherPage";
 
-// import jwt from "jsonwebtoken";
-// require("dotenv").config();
 
-// React Router will help navigate different routes for the vacation time app
-// based on whether the user is logged in or not. 
-// This will prevent users from access unauthorized paths. 
+const Routes = ({ login, signup, updateUser }) => {
 
-  const RoutesApp = ({ login, signup, updateUser }) => {
+  const { isLoggedIn } = useContext(UserContext);
 
-  // const { isLoggedIn } = useContext(UserContext);
-
-  console.debug(
-      "Routes",
-      `login=${typeof login}`,
-      `register=${typeof register}`,
-  );
 
   return (
-    <div className="App">
-      <Routes>
+    <div className="pt-5">
+      {/* <Routes>
         <Route index path="/" element={<Homepage />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/programs" element={<Programs />} />
@@ -50,71 +31,42 @@ import UserPage from "../auth/UserPage"
         <Route path="/contact" element={<Contact />} />
         <Route path="/signup" element={<SignupForm signup={signup} />} />
         <Route path="/login" element={<LoginForm login={login} />} />
-        {/* <Route path="/profile" element={<UserPage />} /> */}
-        {/* <Route path="/update" element={<ProfileForm updateUser={updateUser} />} /> */}
-      </Routes>
-    </div>
+        <Route path="/profile" element={<UserPage />} />
+        <Route path="/update" element={<ProfileForm updateUser={updateUser} />} />
+      </Routes> */}
+
+    <Switch>
+      {isLoggedIn
+        ? (<>
+        <Route index path="/" component={<Homepage />} />
+        <Route path="/aboutus" component={<AboutUs />} />
+        <Route path="/programs" component={<Programs />} />
+        <Route path="/business" component={<Business />} />
+        <Route path="/publications" component={<Publications />} />
+        <Route path="/contact" component={<Contact />} />
+        <Route path="/profile" component={<UserPage />} />
+        <Route path="/update" component={<ProfileForm updateUser={updateUser} />} />
+        <Route path="/weather" component={WeatherPage} />
+        <Redirect to="/" /> 
+      </>) : (<>
+        <Route index path="/" component={<Homepage />} />
+        <Route path="/aboutus" component={<AboutUs />} />
+        <Route path="/programs" component={<Programs />} />
+        <Route path="/business" component={<Business />} />
+        <Route path="/publications" component={<Publications />} />
+        <Route path="/contact" component={<Contact />} />
+        <Route path="/signup" component={<SignupForm signup={signup} />} />
+        <Route path="/login" component={<LoginForm login={login} />} />
+        <Route path="/weather" component={WeatherPage} />
+        <Redirect to="/" />
+      </>)
+    }
+   </Switch>
+
+ </div>
   );
 }
-        {/* </Router> */}
-        {/* <Switch>
-        {isLoggedIn
-                ? (<>
-          <Route exact path="/">
-            <Homepage />
-          </Route>
-          <Route exact path="/company">
-            <AboutUs />
-          </Route>
-         <Route exact path="/programs">
-             <Programs />
-          </Route>
-          <Route exact path="/business">
-             <Business />
-          </Route>
-          <Route exact path="/publications">
-             <Publications />
-          </Route>
-          <Route exact path="/contact">
-             <Contact />
-          </Route>
-          <Route path="/profile">
-            <UserPage />
-          </Route>
-          <Route path="/update">
-              <ProfileForm updateUser={updateUser} />
-            </Route>
-            <Route path="/" element={<Navigate replace to="/" />} />
-          </>) : (<>
-            <Route exact path="/">
-            <Homepage />
-          </Route>
-            <Route exact path="/company">
-            <AboutUs />
-          </Route>
-         <Route exact path="/programs">
-             <Programs />
-          </Route>
-          <Route exact path="/business">
-             <Business />
-          </Route>
-          <Route exact path="/publications">
-             <Publications />
-          </Route>
-          <Route exact path="/contact">
-             <Contact />
-          </Route>
-          <Route exact path="/login">
-            <LoginForm login={login} />
-          </Route>
-          <Route exact path="/signup">
-            <SignupForm signup={signup} />
-          </Route>
-          <Route path="/" element={<Navigate replace to="/" />} />
-          </>)
-            }
-        </Switch> */}
      
 
 
-export default RoutesApp;
+export default Routes;
